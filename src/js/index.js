7f4a7735b5ff306e7f49f89b8242c9ba9919eb2d
Mykola <mgrosscorp@gmail.com>
2018-12-17 23:10:07 +0200

    feature(home): Added home page relativity.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import '../scss/index.scss';
+
+/**
+ *
+ * Toggle class function
+ *
+ * @param element
+ * @param className
+ */
+function toggleClass(element, className) {
+  document.querySelector(element).classList.toggle(className);
+}
+
+// Home page mobile menu toggle
+document.querySelector('.hamburger').addEventListener('click', () => {
+  toggleClass('.home-nav', 'show');
+  toggleClass('.hamburger', 'is-active');
+  toggleClass('.mobile-title', 'show');
+});
+
